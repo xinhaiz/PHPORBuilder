bf3c33d9274ab48f52024cb200070660e5f52e6f
Gsinhi <zxh312007@gmail.com>
2014-09-18 18:17:08 +0800

    Updated, linux 下输入密码不再回显

--- Lib/Params.php
+++ Lib/Params.php
@@ -87,11 +87,17 @@ final class Params {
             if ($option === '+p') {
                 if(strpos($val, '+') === 0) {
                     echo 'Enter password: ';
-                    $openHandle = fopen("php://stdin", "r+");
-                    $useInput   = trim(fread($openHandle, 256));
-                    fclose($openHandle);
 
-                    $this->setOption($option, $useInput);
+                    if(strcasecmp(PHP_OS, 'linux') === 0) {
+                        $passwd = shell_exec('stty -echo && read password && stty echo && echo $password');
+                        echo "\n";
+                    } else {
+                        $openHandle = fopen("php://stdin", "r+");
+                        $passwd   = trim(fread($openHandle, 256));
+                        fclose($openHandle);
+                    }
+
+                    $this->setOption($option, trim($passwd));
                     $option = $val;
                     continue;
                 }
